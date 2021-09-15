8490eebd1b7aa7a980597150b4e1e8936fba307a
RobzLegz <robzlegz@gmail.com>
2021-09-15 11:14:15 +0300

    small update

--- src/components/popups/proffessions/ProffessionPopup.jsx
+++ src/components/popups/proffessions/ProffessionPopup.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { proffessionData } from '../../../slices/proffessions/proffessionSlice'
 import "./ProffessionPopup.scss"
 
 function ProffessionPopup() {
+    const proffessionInfo = useSelector(proffessionData);
+
     return (
-        <div>
-            
+        <div className="proffessionPopup">
+            <ul className="proffessionPopup__categoryList">
+                {proffessionInfo && proffessionInfo.categories.map((category, i) => (
+                    <li key={i}>{category}</li>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/logic/user/info/updateProfileInfo.js
+++ src/logic/user/info/updateProfileInfo.js
@@ -2,6 +2,53 @@ import axios from "axios";
 import { handleLoading, resetLoadingState } from "../../../slices/loading/loadingSlice";
 import { USER_PROFILE } from "../../api/apiRoutes";
 
+export const updateMainInfo = (
+    profileID,
+    avatar,
+    bio,
+    birthDate,
+    country,
+    city,
+    proffessionID,
+    activeJobSeeker,
+    dispatch,
+    accessToken,
+    userInfo
+) => {
+    dispatch(handleLoading(true));
+
+    const formData = new FormData();
+    formData.append("photo", avatar);
+    formData.append("bio", bio);
+    formData.append("birth_date", birthDate);
+    formData.append("country", country);
+    formData.append("city", city);
+    formData.append("interests", userInfo.info.pofile.interests);
+    formData.append("experience", userInfo.info.pofile.experience);
+    formData.append("languages", userInfo.info.pofile.languages);
+    formData.append("knowledge", userInfo.info.pofile.knowledg);
+    formData.append("extra", userInfo.info.pofile.extra);
+    formData.append("profession_aka_activity", proffessionID);
+    formData.append("is_active_jobseeker", activeJobSeeker);
+
+    const headers = {
+        headers: {
+            Authorization: `Token ${accessToken}`,
+            "Content-Type": "multipart/form-data"
+        }
+    }
+
+    axios.put(
+        `${USER_PROFILE}/${profileID}/`, 
+        formData,
+        headers
+    ).then((res) => {
+        dispatch(resetLoadingState());
+    }).catch((err) => {
+        dispatch(resetLoadingState());
+    });
+}
+
 export const updateProfileInfo = (
     profileID,
     avatar,
