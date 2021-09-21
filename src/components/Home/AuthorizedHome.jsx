30e6ed32ae6b68f2866e816da28735e3b50f35d8
Kurstch <61703543+Kurstch@users.noreply.github.com>
2021-09-21 20:22:05 +0300

    Don't check #ADM for company

--- src/components/Home/AuthorizedHome.jsx
+++ src/components/Home/AuthorizedHome.jsx
@@ -115,9 +115,9 @@ function AuthorizedHome() {
                             <div className="username-wrapper">
                                 <h2>
                                     {userInfo.info.is_employer ?
-                                    userInfo.info.profile.company_name :
-                                    <>{userInfo.info.first_name} {userInfo.info.last_name}</>}
-                                    <span>{userInfo.info.profile.is_active_jobseeker && '#AMD'}</span>
+                                        userInfo.info.profile.company_name :
+                                        <>{userInfo.info.first_name} {userInfo.info.last_name} {userInfo.info.profile.is_active_jobseeker && <span>#AMD</span>}</>
+                                    }
                                 </h2>
                                 {userInfo.info.profile.user_proffession_category && userInfo.info.profile.user_proffession ? (
                                     <small>{`${userInfo.info.profile.user_proffession_category} | ${userInfo.info.profile.user_proffession}`}</small>
