dc20822fb523dfe8b615b66787a73b8629a50466
RobzLegz <robzlegz@gmail.com>
2021-09-21 09:44:52 +0300

    #15 follow display

--- src/components/Home/JobsPanel/ScrollJobs.jsx
+++ src/components/Home/JobsPanel/ScrollJobs.jsx
@@ -1,43 +1,17 @@
 import React from "react";
 import './ScrollJobs.scss';
-import OptionsIcon from '../../../assets/svg/options-icon-no-background.svg';
-import Bookmark1 from '../../../assets/svg/bookmark1.svg';
+import ScrollJob from "./scrolljob/ScrollJob";
 
 function ScrollJobs({jobs}) {
     return (
         <>
             {jobs && jobs.map((jobOffer, i) => {
-                let companyInfo = {
-                    logo: "https://tse4.mm.bing.net/th?id=OIP.v-Kd-ya8T4msyd4Gk2VpzgHaHa&pid=Api",
-                    name: "datorium"
-                }
         
                 return (
-                    <div className="job-panel panel" key={i}>
-                        <div className="job-panel__top">
-                            <img src={companyInfo.logo} alt="logo" className="logo" />
-                            <div className="info">
-                                <h4>{companyInfo.name}</h4>
-                                <small>{jobOffer.post_time}</small>
-                            </div>
-                            <button>+ Sekot</button>
-                            <img src={OptionsIcon} alt="options" className="options" />
-                        </div>
-                        <div className="job-panel__location">
-                            <p>{jobOffer.position_city}, {jobOffer.position_country}</p>
-                        </div>
-                        <div className="job-panel__requirements">
-                            <p>{jobOffer.position_requirements}</p>
-                        </div>
-                        <p>{jobOffer.position_info}</p>
-                        <div className="job-panel__bottom">
-                            <img src={Bookmark1} alt="bookmark" />
-                            <div className="job-panel__bottom__price-wrapper">
-                                <small>SĀKOT NO</small>
-                                <h2>€ {jobOffer.price_range}/mēnesī</h2>
-                            </div>
-                        </div>
-                    </div>
+                    <ScrollJob 
+                        jobOffer={jobOffer}
+                        key={i}
+                    />
                 )
             })}
         </>
---
+++ src/components/Home/JobsPanel/scrolljob/ScrollJob.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Bookmark1 from '../../../../assets/svg/bookmark1.svg';
+import OptionsIcon from '../../../../assets/svg/options-icon-no-background.svg';
+
+function ScrollJob({jobOffer}) {
+    const [following, setFollowing] = useState(false);
+
+    let companyInfo = {
+        logo: "https://tse4.mm.bing.net/th?id=OIP.v-Kd-ya8T4msyd4Gk2VpzgHaHa&pid=Api",
+        name: "datorium"
+    }
+
+    return (
+        <div className="job-panel panel">
+            <div className="job-panel__top">
+                <img src={companyInfo.logo} alt="logo" className="logo" />
+                <div className="info">
+                    <h4>{companyInfo.name}</h4>
+                    <small>{jobOffer.post_time}</small>
+                </div>
+                <button onClick={() => setFollowing(!following)} className={following ? "job-panel__top__following" : "job-panel__top__notFollowing"}>{following ? "- Atsekot" : "+ Sekot"}</button>
+                <img src={OptionsIcon} alt="options" className="options" />
+            </div>
+            <div className="job-panel__location">
+                <p>{jobOffer.position_city}, {jobOffer.position_country}</p>
+            </div>
+            <div className="job-panel__requirements">
+                <p>{jobOffer.position_requirements}</p>
+            </div>
+            <p>{jobOffer.position_info}</p>
+            <div className="job-panel__bottom">
+                <img src={Bookmark1} alt="bookmark" />
+                <div className="job-panel__bottom__price-wrapper">
+                    <small>SĀKOT NO</small>
+                    <h2>€ {jobOffer.price_range}/mēnesī</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ScrollJob
