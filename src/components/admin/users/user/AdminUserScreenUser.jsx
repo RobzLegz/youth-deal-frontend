86acd414e49e8036b38a006d31265bfb86c7ab42
Rob <robzlegz@gmail.com>
2021-09-23 17:11:40 +0300

    delete user

--- src/components/admin/users/user/AdminUserScreenUser.jsx
+++ src/components/admin/users/user/AdminUserScreenUser.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux'
+import { deleteUser } from '../../../../logic/user/profile';
+import { userData } from '../../../../slices/user/userSlice'
 
 function AdminUserScreenUser({user}) {
+    const userInfo = useSelector(userData);
+    const dispatch = useDispatch();
+
     return (
         <div>
             {user.is_employer ? (
                 <div>
                     <img src={user.profile.logo} alt="profile" />
                     <h4>{user.profile.company_name}</h4>
-                    {user.is_admin && (<h3>Admin</h3>)}
                 </div>
             ) : (
                 <div>
                     <img src={user.profile.photo} alt="profile" />
                     <h4>{user.first_name}</h4>
-                    {user.is_admin && (<h3>Admin</h3>)}
                 </div>
             )}
+            {user.is_admin && (<h3>Admin</h3>)}
+            <button onClick={() => deleteUser(user.id, userInfo.token, dispatch)}>Delete user</button>
         </div>
     )
 }
