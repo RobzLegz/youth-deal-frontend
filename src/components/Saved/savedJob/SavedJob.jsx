92f887c6815eefaa257769b1820f5d86efc38e56
Rob <robzlegz@gmail.com>
2021-09-27 20:06:33 +0300

    saved display

---
+++ src/components/Saved/savedJob/SavedJob.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { getCompanyInfoById } from '../../../logic/company/info/companyInfo';
+import { getPossitionByID } from '../../../logic/company/positions/positions';
+
+function SavedJob({info}) {
+    const [companyInfo, setCompanyInfo] = useState(null);
+    const [jobOfferInfo, setJobOfferInfo] = useState(null);
+
+    useEffect(() => {
+        if(!companyInfo){
+            getCompanyInfoById(info.company, setCompanyInfo)
+        }
+    }, [companyInfo, info]);
+
+    useEffect(() => {
+        if(!jobOfferInfo && info){
+            getPossitionByID(info.position, setJobOfferInfo);
+        }
+        console.log(jobOfferInfo)
+        console.log(companyInfo)
+    }, [jobOfferInfo, info]);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SavedJob
