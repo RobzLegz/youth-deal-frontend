fb4319c852a9cb798a7d7e19875712d755e47985
RobzLegz <robzlegz@gmail.com>
2021-09-28 12:39:05 +0300

    getting user data

--- src/components/Home/swipedUser/SwipedUser.jsx
+++ src/components/Home/swipedUser/SwipedUser.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getJobOfferUserinfo, getJobseekerInfo } from '../../../logic/user/info/getUserInfo';
 
 function SwipedUser({info}) {
-    console.log(info);
+    const [userInfo, setUserInfo] = useState(null);
+    const [userBaseInfo, setUserBaseInfo] = useState(null);
 
-    return (
-        <div>
-            
-        </div>
-    )
+    useEffect(() => {
+        if(!userInfo){
+            getJobseekerInfo(info.jobseeker, setUserInfo);
+        }else if(userInfo && !userBaseInfo){
+            getJobOfferUserinfo(userInfo.user, setUserBaseInfo)
+        }
+    }, [userInfo, info.jobseeker, userBaseInfo]);
+
+    console.log(userBaseInfo)
+
+    if(userInfo && userBaseInfo){
+        return (
+            <div>
+                <img src={userInfo.photo} alt="user" />
+                <h3>{userBaseInfo.first_name} {userBaseInfo.last_name}</h3>
+            </div>
+        )
+    }
+    return null;
 }
 
 export default SwipedUser
