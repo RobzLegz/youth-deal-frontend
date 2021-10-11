e62abddbf9ffab529a82695b34c96744586fe224
Realman <realman.23@inbox.lv>
2021-10-11 19:39:32 +0300

    courses

--- src/components/courses/course/Course.tsx
+++ src/components/courses/course/Course.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../courses.scss'
+import Star from '../../../assets/svg/Profile/star.svg';
 
 interface Props {
     data: any;
@@ -7,12 +8,13 @@ interface Props {
 
 const Course: React.FC<Props> = ({data}): JSX.Element => {
     return (
-        <div className="courses">
-            <div className="courses__course">
-                <h3>{data.name}</h3>
+        <div className="courses swiper">
+            <div className="courses__course swiper-slide">
                 <img src={data.image} alt={`course with name ${data.name}`} />
-                <p>Author: {data.author}</p>
-                <h4>Price: <u>{data.price} stars</u></h4>
+                <p className="courses__course__name">{data.name}</p>
+                <p className="courses__course__author">{data.author}</p>
+                <p className="courses__course__description">{data.description}</p>
+                <h4>Price: <img src={Star}/>{data.price}</h4>
                 <button>Apply</button>
             </div>
         </div>
@@ -20,3 +22,5 @@ const Course: React.FC<Props> = ({data}): JSX.Element => {
 }
 
 export default Course
+
+
